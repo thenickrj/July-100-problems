9b702e34976e0b62a041cb972ebaa6e2b5ec5af6
Nikhil Singh <nikhilbangalor@gmail.com>
2022-07-03 12:26:00 +0530

    july 3

--- Two Sum.js
+++ Two Sum.js
@@ -1,16 +1,26 @@
 let arr = [1, 3, 3, 4];
+let arr2 = [8, 2, 10, 4, 1, 3];
+let arr3 = [2, 4, 2, 3, 2];
 
-console.log(sum(arr, 5));
+// console.log(sum(arr, 5));
+// console.log(sum(arr2, 9));
+console.log(sum(arr3, 7));
 
 function sum(arr, target) {
   let values = {};
-  for (let i of arr) {
-    console.log(values);
-    if (target - i in values) {
-      return [i, target - i];
-    } else {
-      values[i] = target - i;
+  console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.includes(target - arr[i])) {
+      return [i, arr.indexOf(target - arr[i])];
     }
   }
+  //   for (let i of arr) {
+  //     console.log(values);
+  //     if (target - i in values) {
+  //       return [i, target - i];
+  //     } else {
+  //       values[i] = target - i;
+  //     }
+  //   }
   return false;
 }
